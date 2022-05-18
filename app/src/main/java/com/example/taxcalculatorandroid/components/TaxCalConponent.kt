43239e38bc2f3643b22b03ac96ae4a6335ab449d
MageeMoose <57415911+MageeMoose@users.noreply.github.com>
2022-05-18 23:35:55 +0200

    Added a dropdown component

--- app/src/main/java/com/example/taxcalculatorandroid/components/TaxCalConponent.kt
+++ app/src/main/java/com/example/taxcalculatorandroid/components/TaxCalConponent.kt
@@ -1,18 +1,34 @@
 package com.example.taxcalculatorandroid.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.compose.material.ExposedDropdownMenuDefaults.TrailingIcon as TrailingIcon
 
 
 @ExperimentalComposeUiApi
@@ -59,5 +75,43 @@ fun TexCalButton(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
+@Preview
 @Composable
-fun RegionDropDown(){}
+fun RegionDropDown(){
+    var expanded by remember{mutableStateOf(false)}
+    val testList = listOf("Gävle", "Sundsvall", "Malmö")
+    var selectedRegionText by remember { mutableStateOf("")}
+    var dropDownWidth by remember { mutableStateOf(0)}
+    val icon = if(expanded)
+        Icons.Filled.ArrowDropUp
+    else
+        Icons.Filled.ArrowDropDown
+
+    Column() {
+              OutlinedTextField(
+                value = selectedRegionText,
+                onValueChange = { selectedRegionText = it },
+                  modifier = Modifier
+                      .fillMaxWidth()
+                      .onSizeChanged {
+                          dropDownWidth = it.width
+                      },
+                label = {Text(text = "Region")},
+                trailingIcon = {Icon(icon, "contentDescription",
+                    Modifier.clickable { expanded = !expanded }
+                )}
+              )
+
+          DropdownMenu(expanded = expanded, onDismissRequest = {expanded = false },
+              modifier = Modifier.width(with(LocalDensity.current){dropDownWidth.toDp()})) {
+                testList.forEach { selectedRegion ->
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedRegionText = selectedRegion
+                            expanded = false}) {
+                        Text(text = selectedRegion)}
+                }
+            }
+        }
+}
